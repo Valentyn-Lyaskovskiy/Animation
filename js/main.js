e4ccaa943bc166f10de480c408491f650ba9029f
Valentyn <riodemouro2017@gmail.com>
2023-02-21 00:42:45 +0200

    It was tough!!!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-$(function(){
-
-
-
-
+$(function () {
+  $(".wrapper").hover(
+    function () {
+      $(".img").addClass("mavic-away").removeClass("mavic-back");
+      $(".wrapper").addClass("animation-on");
+    },
+    function () {
+      $(".img").removeClass("mavic-away").addClass("mavic-back");
+      $(".wrapper").removeClass("animation-on");
+    }
+  );
 });
